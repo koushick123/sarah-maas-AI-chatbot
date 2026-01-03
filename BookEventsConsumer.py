2fb7b13e03efe40aa7872c283dfdcf9505b34c78
Koushick Suriyanarayanan <skoushicksuri@gmail.com>
2026-01-03 12:20:45 +0000

    Fix issue with Book events consumer accessing pages_for_OCR file path. Configured kafka, vault and Book events consumer containers to be added to sm-network

--- BookEventsConsumer.py
+++ BookEventsConsumer.py
@@ -17,31 +17,31 @@ def createMongoClient():
     return db['sarah-maas-map-page-nos-chapter-heading']
 
 def book_events_consumer(worker_id):
-        
-    # Configuration for 2025 KRaft and High-Latency OCR
-    conf = {
-        'bootstrap.servers': 'kafka-local:9094',
-        'group.id': 'test-group-2025',
-        'auto.offset.reset': 'earliest',
-        'enable.auto.commit': True,
-        'partition.assignment.strategy': 'roundrobin',
-        
-        # 1. Session Timeout: Detects network/crashes
-        'session.timeout.ms': 45000,
-        'heartbeat.interval.ms': 15000, # 1/3 of session timeout
-        
-        # 2. Max Poll Interval: Time allowed for OCR processing
-        # Increase this if your OCR takes more than 5 minutes per batch
-        'max.poll.interval.ms': 300000, # 5 minutes
-        
-        'error_cb': lambda err: print(f"Worker {worker_id} Error: {err}")
-    }
 
-    c = Consumer(conf)
-    c.subscribe(['mytopic'])
-    print(f"Worker {worker_id} started...")
+    try: 
+        # Configuration for 2025 KRaft and High-Latency OCR
+        conf = {
+            'bootstrap.servers': 'kafka-local:9094',
+            'group.id': 'test-group-2025',
+            'auto.offset.reset': 'earliest',
+            'enable.auto.commit': True,
+            'partition.assignment.strategy': 'roundrobin',
+            
+            # 1. Session Timeout: Detects network/crashes
+            'session.timeout.ms': 45000,
+            'heartbeat.interval.ms': 15000, # 1/3 of session timeout
+            
+            # 2. Max Poll Interval: Time allowed for OCR processing
+            # Increase this if your OCR takes more than 5 minutes per batch
+            'max.poll.interval.ms': 300000, # 5 minutes
+            
+            'error_cb': lambda err: print(f"Worker {worker_id} Error: {err}")
+        }
 
-    try:
+        c = Consumer(conf)
+        c.subscribe(['mytopic'])
+        print(f"Worker {worker_id} started...")
+    
         while True:
             msg = c.poll(5.0)
             if msg is None: continue
@@ -59,17 +59,20 @@ def book_events_consumer(worker_id):
             import json
             val = json.loads(raw_payload) 
             
-            print(f"Processing page: {val['page_num']}")
+            print(f"Processing page: {val['page_num']} for image path {'/'+val['image_path']}")
             
             # 3. Use the parsed dictionary
-            image_text = read_text_from_cropped_ocr_image(val['image_path'], val['page_num'])
+            image_text = read_text_from_cropped_ocr_image('/'+val['image_path'], val['page_num'])
             print(f"image text for page {val['page_num']}: {image_text[:5]}...")
             createMongoClient().insert_one({"book_id": val['book_id'], "page_num": val['page_num'], "extracted_text": image_text[:15]})
 
+    except Exception as e:
+        print(f"Worker {worker_id} Exception: {str(e)}")
     finally:
         c.close()
 
 if __name__ == "__main__":
+    print("Starting multiple Book Events Consumers...")
     num_consumers = 10 
     processes = []
 
--- docker-run-consumer.sh
+++ docker-run-consumer.sh
@@ -1,3 +1,9 @@
 docker container stop book-events-consumer
 docker container rm -f book-events-consumer
-docker run -d --name book-events-consumer --network sm-network koushick123/book-events-consumer:1.0
+if [ "$1" != "local" ]; then
+   echo "Running with codespaces configuration"
+   docker run -d --name book-events-consumer --network sm-network -v /workspaces/sarah-maas-AI-chatbot/pages_for_OCR:/pages_for_OCR koushick123/book-events-consumer:1.0
+else
+   echo "Running with local configuration"
+   docker run -d --name book-events-consumer --network sm-network -v /home/koushick/Young-Adult-ChatBot/pages_for_OCR:/pages_for_OCR koushick123/book-events-consumer:1.0
+fi
